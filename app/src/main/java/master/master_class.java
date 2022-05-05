79662089f8f4815779db21649fa8c1d1f21316b4
amdadul-haque <amdadul1807107@gmail.com>
2022-05-05 21:33:30 +0600

    java class added from master

---
+++ app/src/main/java/master/master_class.java
@@ -0,0 +1,24 @@
+public class master_class{
+  int roll;
+  String name;
+
+  master_class(String a, int r){
+  this.roll = r;
+  this.name = a;
+  }
+
+  public int getRoll(){
+    return roll;
+  }
+  public void setRoll(int roll){
+    this.roll = roll;
+  }
+  public int getName(){
+    return name;
+  }
+  public void setName(int name){
+    this.name = name;
+  }
+
+}
+
