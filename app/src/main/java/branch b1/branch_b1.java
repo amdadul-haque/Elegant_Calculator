a3fd1e64d2512a0413acad53c01296bb2565ab96
amdadul-haque <amdadul1807107@gmail.com>
2022-05-05 22:09:37 +0600

    modified b1 from b2 master

--- app/src/main/java/branch b1/branch_b1.java
+++ app/src/main/java/branch b1/branch_b1.java
@@ -2,9 +2,12 @@ public class branch_b1{
   String name;
   float cgpa;
 
-  branch_b1(String s, float c){
+  int number_of_classes;
+
+  branch_b1(String s, float c,int n){
   this.name = s;
   this.cgpa = c;
+  this.number_of_classes = n;
   }
 
   public float getCgpa(){
@@ -19,6 +22,11 @@ public class branch_b1{
   public void setName(String name){
     this.name = name;
   }
-
+  public int getNumber_of_classes(){
+    return number_of_classes;
+  }
+  public void setNumber_of_classes(int number_of_classes){
+    this.number_of_classes = number_of_classes;
+  }
 }
 
